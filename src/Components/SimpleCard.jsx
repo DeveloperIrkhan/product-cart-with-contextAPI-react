116b0537fb2ca5c57ef48969fb29c00f07dcb008
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2024-07-16 16:15:55 +0400

    shoping cart functionallity added

--- src/Components/SimpleCard.jsx
+++ src/Components/SimpleCard.jsx
@@ -4,14 +4,14 @@ import CartContext from '../Context/CartContext';
 const SimpleCard = ({ products }) => {
   const { cart, setCart } = useContext(CartContext)
   const isInCart = cart.some((item) => item.id === products.id);
-  // const handleAddToCart = () => {
-  //   setCart((prevCart) => [...prevCart, product]);
-  // };
+  const handleAddToCart = () => {
+    setCart((prevCart) => [...prevCart, products]);
+  };
 
-  // const handleRemoveFromCart = () => {
-  //   setCart((prevCart) => prevCart.filter((item) => item.id !== product.id));
-  // };
-console.log("Cart Context",useContext(CartContext))
+  const handleRemoveFromCart = () => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== products.id));
+  };
+  console.log("Cart Context", useContext(CartContext))
   return (
     <div className="card m-2" key={products.id} style={{ width: '18rem' }}>
       <img src={products.image} style={{ height: "40vmin" }} className="card-img-top" alt="..." />
@@ -20,17 +20,12 @@ console.log("Cart Context",useContext(CartContext))
         <p className="card-text">Price : {products.price}</p>
         {isInCart ? (
           <button className="btn btn-outline-success"
-            onClick={() => {
-              setCart((prevCart) => prevCart.filter((item) => item.id !== products.id)
-              )
-            }}>
+            onClick={handleRemoveFromCart}>
             Remove item Cart
           </button>
         ) : (
           <button className="btn btn-success"
-            onClick={() => {
-              setCart((prevCart) => [...prevCart, products]);
-            }}>
+            onClick={handleAddToCart}>
             Add to Cart
           </button>
         )}
